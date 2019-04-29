10024f6454199566e0432cb1a7144c9475424bab
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-04-29 17:03:29 -0400

    Update sharkbot.js

--- sharkbot.js
+++ sharkbot.js
@@ -1,12 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true});
+const config = require("./config.json");
 
-client.on('message', message => {
-    if(message.content.startsWith("."))
+client.on("message", async message => {
     if(!message.member.roles.some(r=>["bot-user"].includes(r.name)) )
         return message.reply("I'm not saying that.");
+    if(message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if(cmd === "say") {
+        
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
-})
+  }
+    
+});
+
 client.login(process.env.BOT_TOKEN);
