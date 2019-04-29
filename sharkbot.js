6fd99d2059ed2edfdfe925bd692281cb296836f4
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-04-29 17:07:08 -0400

    Update sharkbot.js

--- sharkbot.js
+++ sharkbot.js
@@ -4,10 +4,10 @@ const config = require("./config.json");
 
 client.on("message", async message => {
     if(!message.member.roles.some(r=>["bot-user"].includes(r.name)) )
-        return message.reply("I'm not saying that.");
+        return
     if(message.author.bot) return;
     if(message.content.indexOf(config.prefix) !== 0) return;
-    const args = message.content.se(config.prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     
     if(cmd === "say") {
